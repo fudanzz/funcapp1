89ae75534ecd2abc5eee6373f7d65057957d1476
Phil Zhao <zhongzhao@microsoft.com>
2022-02-16 14:57:28 +0800

    ''

--- helloworld.cs
+++ helloworld.cs
@@ -27,7 +27,7 @@ namespace openhack
 
             string responseMessage = string.IsNullOrEmpty(productID)
                 ? "pls provide product id for your query!"
-                : $"The product name for your product id {productID} is Starfruit Explosion test";
+                : $"The product name for your product id {productID} is Starfruit Explosion test123";
 
             return new OkObjectResult(responseMessage);
         }
